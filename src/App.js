5b341964896c82d0075a759151fa6a0128714ba8
Sana Khemir <archi.khemirsana@gmail.com>
2024-12-24 20:29:03 +0100

    changement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import Name from './Name';
+import Price from './Price';
+import Description from './Description';
+import Image from './Image';
+
+
 
 function App() {
+
+  const firstName = "Sana";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Name />
+        <Price />
+        <Image />
+        <Description />
+      </div>
+
+      <div>
+        <h4>{`Bonjour, ${firstName}!`}</h4>
+      </div>
     </div>
   );
 }
---
+++ src/product.js
@@ -0,0 +1,9 @@
+
+const product = { 
+    Name : "jeu défi nature", 
+    Price: "20,00 €",
+    Description : " un jeux offre aux joueurs une expérience de jeu encore plus immersive pour découvrir notre Planète !",
+    imageUrl : "https://www.defis-nature-protect.com/wp-content/uploads/2024/05/card-background.png"
+};
+
+export default product;
